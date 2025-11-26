9682dfbe3b4f9148f286a88fb9fad86ef0a1babb
siddhimh <siddhi2272@gmail.com>
2025-11-26 03:51:32 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import About from "./components/about";
+import Skills from "./components/skills";
+import Work from "./components/work-experience";
+import Contact from "./components/contacts";
+import Projects from "./components/projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Projects />
+      <Contact />
     </div>
   );
 }
